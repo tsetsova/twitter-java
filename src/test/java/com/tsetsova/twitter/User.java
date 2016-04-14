04060dc6760416e5715d9fd2365e935e87dc62ba
Ina <inatsetsova@gmail.com>
2016-04-14 17:56:08 +0100

    creates a user with username

---
+++ src/test/java/com/tsetsova/twitter/User.java
@@ -0,0 +1,15 @@
+package com.tsetsova.twitter;
+
+
+ class User {
+
+    private String username;
+
+    User(String username) {
+        this.username = username;
+    }
+
+    String name() {
+        return username;
+    }
+}
---
+++ src/test/java/com/tsetsova/twitter/UserTest.java
@@ -0,0 +1,18 @@
+package com.tsetsova.twitter;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+    @Test
+    public void shouldReturnName(){
+
+        User spike = new User("Spike");
+        assertEquals("Spike", spike.name() );
+
+    }
+
+}
