124295fff004a79e2b6cf56de870b9b33b3d0033
Ina <inatsetsova@gmail.com>
2016-04-17 19:58:32 +0100

    refactors user_interaction into separate methods

--- src/main/java/com/tsetsova/twitter/Twitter.java
+++ src/main/java/com/tsetsova/twitter/Twitter.java
@@ -4,76 +4,104 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Twitter {
 
-   List<User> users = new ArrayList<>();
-   List<String>commands = Arrays.asList("tweet", "timeline","search <username>","sign out", "exit");
-   User current_user;
+    List<User> users = new ArrayList<>();
+    List<String> commands = Arrays.asList("tweet", "timeline", "search <username>", "sign out", "exit");
+    User current_user;
+    Scanner reader = new Scanner(System.in).useDelimiter("\\n");
 
     public static void main(String[] arguments) {
-       new Twitter().user_interaction();
-       System.out.println("See you later!");
-   }
+        new Twitter().user_interaction();
+        System.out.println("See you later!");
+    }
 
     private void user_interaction() {
-        Scanner reader = new Scanner(System.in).useDelimiter("\\n");
-        System.out.println("Hi, to tweet, please enter your username:");
-        String username = reader.next().replaceAll("\\s","");
 
-        List<String> usernames = users.stream()
-                                      .map(user -> user.username)
-                                      .collect(Collectors.toList());
-
-
-        current_user = users.stream()
-                            .filter(user -> user.username.equalsIgnoreCase(username) )
-                            .findFirst()
-                            .orElse(new User(username));
-
-        if (!usernames.contains(username)) {
-            users.add(current_user);
-        }
-
-        System.out.println("Hello " + username);
-        System.out.println("The commands I understand are:");
-        commands.forEach(System.out::println);
-        String command = reader.next();
-
+        login();
+        String command = userCommand();
 
         while (!command.equalsIgnoreCase("quit")) {
-            switch(command) {
+            switch (command) {
                 case "timeline":
-                    if (current_user.timeline().isEmpty()) {
-                        System.out.println("No tweets yet! Say 'tweet' to tweet!");
-                    }
-                    current_user.timeline().forEach(System.out::println);
+                    timeline();
                     break;
                 case "tweet":
-                    System.out.println("What's on your mind?");
-                    String status = reader.next();
-                    current_user.tweet(status);
-                    String tweet = String.format("@%s said '%s'", username, status);
-                    System.out.println(tweet);
+                    tweet();
                     break;
                 case "sign out":
-                    System.out.println("Signed out, your tweets have been saved.");
-                    user_interaction();
+                    signOut();
+                    break;
                 case "exit":
-                    System.out.println("Are you sure? All your tweets will be lost. Say 'Yes' to confirm exit.");
-                    String exitResponse = reader.next();
-                    if (exitResponse.equalsIgnoreCase("Yes")) {
-                        return;
-                    }
-                    else {
-                        System.out.println("Back to safety.");
-                    }
+                    if (exit()) return;
                     break;
                 default:
-                    System.out.println("Sorry, I only understand: tweet, timeline, and exit currently");
+                    System.out.println("Sorry did you mean one of these commands?");
+                    printCommands();
             }
             command = reader.next();
         }
     }
+
+    private boolean exit() {
+        System.out.println("Are you sure? All your tweets will be lost. Say 'Yes' to confirm exit.");
+        String exitResponse = reader.next();
+        if (exitResponse.equalsIgnoreCase("Yes")) {
+            return true;
+        } else {
+            System.out.println("Back to safety.");
+        }
+        return false;
+    }
+
+    private void timeline() {
+        if (current_user.timeline().isEmpty()) {
+            System.out.println("No tweets yet! Say 'tweet' to tweet!");
+        }
+        current_user.timeline().forEach(System.out::println);
+    }
+
+    private void signOut() {
+        System.out.println("Signed out, your tweets have been saved.");
+        user_interaction();
+    }
+
+    private void tweet() {
+        System.out.println("What's on your mind?");
+        String status = reader.next();
+        current_user.tweet(status);
+        String tweet = String.format("@%s said '%s'", current_user.username, status);
+        System.out.println(tweet);
+    }
+
+
+    private String userCommand() {
+        printCommands();
+        return reader.next();
+    }
+
+    private void printCommands() {
+        System.out.println("The commands I understand are:");
+        commands.forEach(System.out::println);
+    }
+
+    private void login() {
+        System.out.println("Hi, to tweet, please enter your username:");
+        String username = reader.next().replaceAll("\\s", "");
+
+        current_user = users.stream()
+                .filter(user -> user.username.equalsIgnoreCase(username))
+                .findFirst()
+                .orElse(createUser(username));
+
+        System.out.println("Hello " + username);
+    }
+
+    private User createUser(String username) {
+        User user = new User(username);
+        users.add(user);
+        return user;
+    }
+
 }
